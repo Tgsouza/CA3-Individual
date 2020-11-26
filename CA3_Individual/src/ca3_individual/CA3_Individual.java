cedff3851105014a780d0b4556247e27378759d7
Tgsouza <72031342+Tgsouza@users.noreply.github.com>
2020-11-26 17:27:10 +0000

    Main  code

---
+++ CA3_Individual/src/ca3_individual/CA3_Individual.java
@@ -0,0 +1,21 @@
+
+package ca3_individual;
+
+/**
+ *
+ * @author Thiago Gomes de Souza
+ * Student number 2020310
+ * 
+ */
+public class CA3_Individual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World !");
+        //The changes made here will be displayed on GitHub desktop
+    }
+    
+}
